13d01dee53c4818e72e97beda0efbf98fc80ad88
cococo-f <fujikawa.k1130@gmail.com>
2023-04-01 16:24:11 +0900

    サイドバーの【スクール予約確認・スクール枠登録】を講師アカウントにのみ表示を修正

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -23,7 +23,7 @@
       <p><a href="/logout">ログアウト</a></p>
       <p><a href="{{ route('calendar.general.show',['user_id' => Auth::id()]) }}">スクール予約</a></p>
       <!-- 講師アカウントにのみ表示 -->
-      @if (Auth::user()->role < 3)
+      @if (Auth::user()->role < 4 )
       <p><a href="{{ route('calendar.admin.show',['user_id' => Auth::id()])}}">スクール予約確認</a></p>
       <p><a href="{{ route('calendar.admin.setting',['user_id' => Auth::id()]) }}">スクール枠登録</a></p>
       @endif
