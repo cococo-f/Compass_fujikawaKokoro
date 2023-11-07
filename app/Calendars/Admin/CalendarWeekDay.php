8d9071983ececef9d8d8298146bda236cd0a70fe
cococo-f <fujikawa.k1130@gmail.com>
2023-11-07 20:20:21 +0900

    最新

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -29,7 +29,9 @@ class CalendarWeekDay{
     $two_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
     $three_part = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
 
-    $html[] = '<div class="text-left">';
+
+
+    $html[] = '<div class="text-left">{{'.$ymd.'}}';
     if($one_part){
       $html[] = '<p class="day_part m-0 pt-1">1部</p>';
     }
